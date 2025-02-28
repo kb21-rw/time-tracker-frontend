ef8ef8943d4c80fb84f557beef8d075f47201cf6
aimee-annabelle <inezannabelle@gmail.com>
2025-02-28 14:59:15 +0200

    chore: font family configuration

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -3,7 +3,7 @@ import LogoIcon from '../assets/LogoIcon'
 export default function LandingPage() {
     return (
         <div>
-            <h1>Landing page</h1>
+            <h1 className="font-inter">Landing page</h1>
             <LogoIcon />
         </div>
     )
