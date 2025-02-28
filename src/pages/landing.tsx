0816be2b45d2a7d5fbe5ddc6eaf62b2487640001
aimee-annabelle <inezannabelle@gmail.com>
2025-02-28 15:43:06 +0200

    chore: setup colors

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,10 +1,10 @@
-import LogoIcon from '../assets/LogoIcon'
+// import LogoIcon from '../assets/LogoIcon'
 
 export default function LandingPage() {
     return (
         <div>
-            <h1 className="font-inter">Landing page</h1>
-            <LogoIcon />
+            <h1 className="font-inter text-primary">Landing page</h1>
+            {/* <LogoIcon /> */}
         </div>
     )
 }
