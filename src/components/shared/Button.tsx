7614c05413aa7618575826ff2b90e95ff5ad2114
aimee-annabelle <inezannabelle@gmail.com>
2025-02-28 15:58:32 +0200

    feature: button component

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,3 +1,16 @@
-export default function Button(){
-    return <div></div>
+import { ButtonHTMLAttributes } from 'react'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    isLoading?: boolean
+}
+export default function Button({ isLoading, className, ...props }: ButtonProps) {
+    return (
+        <button
+            {...props}
+            className={`bg-primary text-white rounded-lg text-center p-4 ${className} ${isLoading ? 'bg-primary/45' : ''}`}
+            disabled={isLoading}
+        >
+            {isLoading ? 'Loading...' : props.children}
+        </button>
+    )
 }
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,9 +1,14 @@
 // import LogoIcon from '../assets/LogoIcon'
 
+import Button from '../components/shared/Button'
+
 export default function LandingPage() {
     return (
         <div>
             <h1 className="font-inter text-primary">Landing page</h1>
+            <Button className="text-xl w-1/2" isLoading={true} onClick={() => alert('Hello')}>
+                Get started
+            </Button>
             {/* <LogoIcon /> */}
         </div>
     )
