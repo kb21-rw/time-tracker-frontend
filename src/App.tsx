24d9173e76308f850b3dbb5b9b5e52878b5b9875
aimee-annabelle <inezannabelle@gmail.com>
2025-03-03 09:53:28 +0200

    feat: connect landing and signup page and remove linting errors

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
     return (
         <>
             <RouterProvider router={router} />
-           {/* <SignUpPage/> */}
         </>
     )
 }
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,28 +1,35 @@
 import LogoIcon from '../assets/icons/LogoIcon'
 import Button from '../components/shared/Button'
 import LandingImage from '../assets/images/landing-image.svg'
+import { Link } from 'react-router-dom'
 
 export default function LandingPage() {
     return (
         <div className="w-full flex flex-col px-4 py-2 md:px-24 md:py-12 h-screen bg-gray-100">
             <div className="flex justify-between ">
-                <LogoIcon/>
+                <LogoIcon />
                 <p className="text-3xl md:text-5xl font-fredoka font-semibold text-primary text-shadow-custom">
                     Focus<span className="font-inter text-secondary"> Flow</span>
                 </p>
             </div>
 
-            <div className='flex justify-between mt-20 items-center'>
-                <div className='flex flex-col gap-6  '>
-                    <p className='text-5xl font-bold font-inter text-secondary '>
-                        Where focus meets <br/> productivity
+            <div className="flex justify-between mt-20 items-center">
+                <div className="flex flex-col gap-6  ">
+                    <p className="text-5xl font-bold font-inter text-secondary ">
+                        Where focus meets <br /> productivity
                     </p>
-                    <p className='font-inter text-xl'>
-                        Streamlining your day and <br/>making every moment count.
+                    <p className="font-inter text-xl">
+                        Streamlining your day and <br />
+                        making every moment count.
                     </p>
-                    <Button className='w-48'> Get started </Button>
+                    <Link
+                        className="w-48 bg-primary shadow shadow-black/40 drop-shadow-xl text-white rounded-lg text-center p-4"
+                        to="/sign-up"
+                    >
+                        Get started
+                    </Link>
                 </div>
-                <img src={LandingImage} alt="landing page Image"/>
+                <img src={LandingImage} alt="landing page Image" />
             </div>
         </div>
     )
