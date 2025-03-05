f52d09abc4493a83d03f42a4ad9a69ba3a4d5aed
Gihozo23 <christellegihozo23@gmail.com>
2025-03-05 20:53:58 +0200

    feature: implement the form submission to the API

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -6,7 +6,11 @@ import Input from '../components/shared/Input'
 import { signUpSchema } from '../util/validationSchema'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useState } from 'react'
+import { AppDispatch, RootState } from '../redux/store'
+import { useDispatch, useSelector } from 'react-redux'
+import { signupUser } from '../redux/slice/authSlice'
+import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
 
 type FormFields = z.infer<typeof signUpSchema>
 
@@ -18,24 +22,33 @@ const defaultValues: FormFields = {
 }
 
 export default function SignUpPage() {
+    const dispatch = useDispatch<AppDispatch>()
+    const { loading, error } = useSelector((state: RootState) => state.auth)
     const {
         register,
         handleSubmit,
-        reset,
         formState: { errors, isValid },
     } = useForm<FormFields>({
         resolver: zodResolver(signUpSchema),
         mode: 'all',
         defaultValues,
     })
-    const [isSubmitting, setIsSubmitting] = useState(false)
+    const navigate = useNavigate()
 
-    const onSubmit: SubmitHandler<FormFields> = data => {
-        setIsSubmitting(true)
-        alert(`submitted successfully |${data}`)
-        reset()
-        setIsSubmitting(false)
+    const onSubmit: SubmitHandler<FormFields> = async data => {
+        try {
+            const { meta: responseData } = await dispatch(signupUser(data)) // Dispatch signup action
+            if (responseData.requestStatus === 'fulfilled') {
+                navigate('/login')
+                toast.success('Signup successful!')
+            } else {
+                toast.error('Signup failed')
+            }
+        } catch (error) {
+            console.error('An error occured', error)
+        }
     }
+
     return (
         <div className="w-full flex flex-col px-4 py-2 md:px-24 md:py-12 h-screen">
             <div className="w-full flex justify-between items-center">
@@ -82,10 +95,16 @@ export default function SignUpPage() {
                             register={register('confirmPassword')}
                             error={errors.confirmPassword}
                         />
+                        {error && (
+                            <p className="text-red-500 text-sm mt-2">
+                                {typeof error === 'string' ? error : JSON.stringify(error)}
+                            </p>
+                        )}
+
                         <Button
                             className="text-xl w-full mt-6"
-                            isLoading={isSubmitting}
-                            disabled={!isValid || isSubmitting}
+                            isLoading={loading}
+                            disabled={!isValid || loading}
                         >
                             Create Account
                         </Button>
