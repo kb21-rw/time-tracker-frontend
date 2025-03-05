d0b3794e50a873334864e3bdba47ba33e0e43098
Gihozo23 <christellegihozo23@gmail.com>
2025-03-05 20:49:36 +0200

    feature: implement the auth slice in our redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './slice/authSlice'
 import counterReducer from './slice/counterSlice'
 const store = configureStore({
     reducer: {
         counter: counterReducer,
+        auth: authReducer,
     },
 })
 
