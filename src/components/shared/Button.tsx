7d6e38d53c5b84bd445f0cd6fd1f0bbe0dac7e86
manzitresor <manzitre@gmail.com>
2025-03-06 11:26:03 +0200

    chore: add Loading spinner

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,4 +1,5 @@
 import { ButtonProps } from '../../util/interfaces'
+import LoadingSpinner from '../LoadingSpinner'
 
 export default function Button({ isLoading, className, disabled, children }: ButtonProps) {
     return (
@@ -6,7 +7,7 @@ export default function Button({ isLoading, className, disabled, children }: But
             className={`bg-primary shadow shadow-black/40 drop-shadow-xl text-white rounded-lg text-center p-4 ${className} ${isLoading || disabled ? 'bg-primary/50' : ''}`}
             disabled={isLoading || disabled}
         >
-            {isLoading ? 'Loading...' : children}
+            {isLoading ? <LoadingSpinner/> : children}
         </button>
     )
 }
