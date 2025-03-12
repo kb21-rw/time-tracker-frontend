3b90af23b8681a116e65ba2c1956493c3e86680e
aimee-annabelle <inezannabelle@gmail.com>
2025-03-12 12:09:31 +0200

    fix: signup header

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -13,6 +13,7 @@ import { useNavigate } from 'react-router-dom'
 import { handleAxiosError } from '../util/helpers'
 import { AxiosError } from 'axios'
 import { signUpSchema } from '../schema'
+import FocusFlowHeader from '../components/shared/FocusFlowHeader'
 
 type FormFields = z.infer<typeof signUpSchema>
 
@@ -53,12 +54,7 @@ export default function SignUpPage() {
 
     return (
         <div className="w-full flex flex-col px-4 py-2 md:px-24 md:py-12 h-screen">
-            <div className="w-full flex justify-between items-center">
-                <LogoIcon className="w-12 md:w-20" />
-                <h1 className="text-3xl md:text-5xl font-fredoka font-semibold text-primary text-shadow-custom">
-                    Focus<span className="font-inter text-secondary"> Flow</span>
-                </h1>
-            </div>
+            <FocusFlowHeader />
             <div className="flex h-full justify-between items-center">
                 <img
                     src={SignUpImage}
