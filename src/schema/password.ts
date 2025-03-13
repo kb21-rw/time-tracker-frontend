6888bbcf9fa11ef75df057bac3e6672d5f8917ff
aimee-annabelle <inezannabelle@gmail.com>
2025-03-13 15:30:57 +0200

    refactor: update the min length for confirm password in schema

--- src/schema/password.ts
+++ src/schema/password.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 export const passwordSchema = z
     .object({
         password: z.string().min(8, { message: 'Password must be at least 8 characters' }),
-        confirmPassword: z.string().min(1, { message: 'Confirm password is required' }),
+        confirmPassword: z.string().min(8, { message: 'Confirm password is required' }),
     })
     .refine(data => data.password === data.confirmPassword, {
         message: "Passwords don't match",
