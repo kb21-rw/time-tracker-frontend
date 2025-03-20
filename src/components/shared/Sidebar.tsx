9af88f81c66c07ba2b154078b82fba28ca6ccce8
manzitresor <manzitre@gmail.com>
2025-03-20 16:06:00 +0200

    refactor: add link to sidebar images

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -4,23 +4,37 @@ import SidebarImage from '../../assets/images/sidebar-close.svg'
 import Download from '../../assets/images/download_icon.svg'
 import Notification from '../../assets/images/notification-line.svg'
 import Settings from '../../assets/images/settings_icon.svg'
+import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
     return (
         <div>
             <aside className="row-span-5 bg-primary-800 h-screen top-0 bottom-0">
                 <div className="space-y-2 h-full px-2 mb-2 py-4 flex flex-col justify-between">
+                    {/* The links that redirect to specific route will be added later */}
                     <div className="space-y-3">
-                        <img src={Home} className="h-7 w-7" />
-                        <img src={Profile} className="h-7 w-7" />
+                        <Link to="#">
+                            <img src={Home} className="h-7 w-7" />
+                        </Link>
+                        <Link to="#">
+                            <img src={Profile} className="h-7 w-7" />
+                        </Link>
                     </div>
                     <div>
-                        <img src={SidebarImage} className="h-7 w-7" />
+                        <Link to="#">
+                            <img src={SidebarImage} className="h-7 w-7" />
+                        </Link>
                     </div>
                     <div>
-                        <img src={Notification} className="h-7 w-7" />
-                        <img src={Download} className="h-7 w-7" />
-                        <img src={Settings} className="h-7 w-7" />
+                        <Link to="#">
+                            <img src={Notification} className="h-7 w-7" />
+                        </Link>
+                        <Link to="#">
+                            <img src={Download} className="h-7 w-7" />
+                        </Link>
+                        <Link to="#">
+                            <img src={Settings} className="h-7 w-7" />
+                        </Link>
                     </div>
                 </div>
             </aside>
