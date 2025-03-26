a14a4c99a0c307d6fb910c4efbbaddbb44e02dc8
Gihozo23 <christellegihozo23@gmail.com>
2025-03-26 23:02:15 +0200

    feature: implement the formatDate function

--- src/components/card/WorkspaceCard.tsx
+++ src/components/card/WorkspaceCard.tsx
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom'
 import { MdModeEditOutline } from 'react-icons/md'
 import { WorkspaceProps } from '../../util/interfaces'
+import { formatDate } from '../../util/helpers'
 
 export default function WorkspaceCard(props: WorkspaceProps) {
+    const formattedDate = formatDate(props.creationDate)
     return (
         <Link to="/workspace-details">
             <div className="border rounded-full border-gray-200 py-3 md:py-4">
                 <div className="flex justify-between items-center px-4 md:px-11">
                     <p className=" text-sm md:text-lg">{props.name}</p>
                     <div className="flex items-center gap-x-9">
-                        <span>{props.creationDate}</span>
+                        <span>{formattedDate}</span>
                         <button>
                             <MdModeEditOutline />
                         </button>
--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -7,3 +7,7 @@ export const handleAxiosError = (error: AxiosError) => {
         return error.message
     }
 }
+export const formatDate = (isoString: string): string => {
+    const date = new Date(isoString)
+    return date.toLocaleDateString('en-GB')
+}
