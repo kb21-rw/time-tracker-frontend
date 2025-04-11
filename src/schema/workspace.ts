bb5fc3b0c05dd369b31f2b22c78e1998f72050e7
manzitresor <manzitre@gmail.com>
2025-04-11 12:05:11 +0200

    chore: create invite user schema

--- src/schema/workspace.ts
+++ src/schema/workspace.ts
@@ -6,3 +6,11 @@ export const workspaceShema = z.object({
         .min(3, { message: 'name must be at least 3 characters' })
         .regex(/^[A-Z]/, { message: 'name must start with a capital letter' }),
 })
+
+export const inviteUserSchema = z.object({
+    fullName: z.string().refine(value => value.trim().split(/\s+/).length >= 2, {
+        message: 'Please enter your full name (first and last name)',
+    }),
+    
+    email: z.string().email({ message: 'Please enter a valid email address' }),
+})
