3de85173341e45422b12cfe1941745ac1f3fb229
manzitresor <manzitre@gmail.com>
2025-04-16 18:05:22 +0200

    chore: add workspaceUsers in workspace slice

--- src/redux/slice/workspaceSlice.ts
+++ src/redux/slice/workspaceSlice.ts
@@ -4,6 +4,7 @@ import { WorkspaceState } from '../../util/interfaces'
 
 const initialState: WorkspaceState = {
     workspaces: [],
+    workspaceUsers: [],
     loading: false,
     error: null,
 }
@@ -70,6 +71,21 @@ export const renameWorkspace = createAsyncThunk(
     },
 )
 
+export const getWorkspaceUsers = createAsyncThunk(
+    'workspace/getUsers',
+    async(id: string, {rejectWithValue}) => {
+        try {
+            const response = await api.get(`Workspaces/${id}/users`);
+            return response.data;
+        } catch (error:any) {
+                        const errorMessage = error.response?.data?.message || 'Fetching users failed'
+            return rejectWithValue(
+                typeof errorMessage === 'string' ? errorMessage : JSON.stringify(errorMessage),
+            )
+        }
+    }
+)
+
 const workspacesSlice = createSlice({
     name: 'workspace',
     initialState,
@@ -126,6 +142,18 @@ const workspacesSlice = createSlice({
                 state.workspaces[1].name
             })
             .addCase(renameWorkspace.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload
+            })
+            .addCase(getWorkspaceUsers.pending, state => {
+                state.loading = true
+                state.error = null
+            })
+            .addCase(getWorkspaceUsers.fulfilled, (state,action)=> {
+                state.loading = false
+                state.workspaceUsers = action.payload
+            })
+            .addCase(getWorkspaceUsers.rejected,(state,action)=> {
                 state.loading = false
                 state.error = action.payload
             })
