938301d816759932dd81642d3ddc6bad8bf12932
Gihozo23 <christellegihozo23@gmail.com>
2025-04-16 17:20:21 +0200

    feat: create the signupUser thunk

--- src/redux/slice/authSlice.ts
+++ src/redux/slice/authSlice.ts
@@ -26,6 +26,21 @@ export const signupAdmin = createAsyncThunk(
     },
 )
 
+export const signupUser = createAsyncThunk(
+    'workspaces/invitations/accept',
+    async ({ token, password }: { token: string; password: string }, { rejectWithValue }) => {
+        try {
+            const response = await api.post(`workspaces/invitations/accept`, { token, password })
+            return response.data
+        } catch (error: any) {
+            const errorMessage = error.response.data.message || 'Accepting invitation failed'
+            return rejectWithValue(
+                typeof errorMessage === 'string' ? errorMessage : JSON.stringify(errorMessage),
+            )
+        }
+    },
+)
+
 export const loginUser = createAsyncThunk(
     'auth/loginUser',
     async (userData: { email: string; password: string }, { rejectWithValue }) => {
@@ -112,6 +127,19 @@ const authSlice = createSlice({
                 state.token = action.payload.token
             })
             .addCase(signupAdmin.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload
+            })
+            .addCase(signupUser.pending, state => {
+                state.loading = true
+                state.error = null
+            })
+            .addCase(signupUser.fulfilled, (state, action) => {
+                state.loading = false
+                state.user = action.payload.user
+                state.token = action.payload.token
+            })
+            .addCase(signupUser.rejected, (state, action) => {
                 state.loading = false
                 state.error = action.payload
             })
