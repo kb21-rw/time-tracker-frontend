ea5d020ecc93fcba674a6dc4e22021ccfbc684f5
manzitresor <manzitre@gmail.com>
2025-04-28 16:02:03 +0200

    chore: create Edit buttons component

---
+++ src/components/ui/EditButtons.tsx
@@ -0,0 +1,14 @@
+import Button from '../shared/ui/Button'
+
+export default function EditButtons() {
+  return (
+      <div className="flex flex-col px-3 py-4 gap-2">
+          <Button className="w-full text-left px-2 py-2 justify-start font-normal cursor-pointer">
+              Edit Client
+          </Button>
+          <Button className="w-full text-left px-2 py-2 justify-start font-normal cursor-pointer">
+              Edit Project
+          </Button>
+      </div>
+  )
+}
