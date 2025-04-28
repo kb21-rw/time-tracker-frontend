2ba5226b7f7fc3c17d66924e2d4224c0e431705e
manzitresor <manzitre@gmail.com>
2025-04-28 16:03:31 +0200

    chore: add edit buttons and modal in table column

--- src/components/ui/GroupsTableColumns.tsx
+++ src/components/ui/GroupsTableColumns.tsx
@@ -3,6 +3,9 @@
 import { ColumnDef } from '@tanstack/react-table'
 import { GroupTable } from '../../util/interfaces'
 import { Pencil, Plus } from 'lucide-react'
+import { useState } from 'react'
+import EditPopUpModal from '../shared/shared/EditPopUpModal'
+import EditButtons from './EditButtons'
 
 export const groupsTableColumns: ColumnDef<GroupTable>[] = [
     {
@@ -32,12 +35,38 @@ export const groupsTableColumns: ColumnDef<GroupTable>[] = [
         header: 'Actions',
         cell: ({ row }) => {
             const rowData = row.original
+            
+                 const [isPopupOpen, setIsPopupOpen] = useState(false)
+                 const [popupPosition, setPopupPosition] = useState<{
+                     x: number
+                     y: number
+                 } | null>(null)
+
+                 const handleEditClick = (event: React.MouseEvent<SVGSVGElement>) => {
+                     const rect = event.currentTarget.getBoundingClientRect()
+                     setPopupPosition({
+                         x: rect.right + window.scrollX - 20,
+                         y: rect.bottom + window.scrollY + 40,
+                     })
+                     setIsPopupOpen(true)
+                 }
+
+                 const closePopup = () => {
+                     setIsPopupOpen(false)
+                     setPopupPosition(null)
+                 }
             return (
-                <div className="flex items-center justify-center gap-x-4">
-                    <Pencil
-                        onClick={() => console.log(rowData)}
-                        className="text-primary-500 w-5 h-5"
-                    />
+                <div className="flex items-center justify-center gap-x-4 relative">
+                    <Pencil onClick={handleEditClick} className="text-primary-500 w-5 h-5 cursor-pointer" />
+                    {isPopupOpen && popupPosition && (
+                        <EditPopUpModal
+                            isModalOpen={isPopupOpen}
+                            onClose={closePopup}
+                            popupPosition={popupPosition}
+                        >
+                            <EditButtons/>
+                        </EditPopUpModal>
+                    )}
                 </div>
             )
         },
