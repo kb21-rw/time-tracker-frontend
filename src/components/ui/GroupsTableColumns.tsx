d8d366bf5fb805fc57cf341841022baf642a9d06
manzitresor <manzitre@gmail.com>
2025-04-28 16:05:20 +0200

    fix:remove linter errors

--- src/components/ui/GroupsTableColumns.tsx
+++ src/components/ui/GroupsTableColumns.tsx
@@ -35,36 +35,39 @@ export const groupsTableColumns: ColumnDef<GroupTable>[] = [
         header: 'Actions',
         cell: ({ row }) => {
             const rowData = row.original
-            
-                 const [isPopupOpen, setIsPopupOpen] = useState(false)
-                 const [popupPosition, setPopupPosition] = useState<{
-                     x: number
-                     y: number
-                 } | null>(null)
 
-                 const handleEditClick = (event: React.MouseEvent<SVGSVGElement>) => {
-                     const rect = event.currentTarget.getBoundingClientRect()
-                     setPopupPosition({
-                         x: rect.right + window.scrollX - 20,
-                         y: rect.bottom + window.scrollY + 40,
-                     })
-                     setIsPopupOpen(true)
-                 }
+            const [isPopupOpen, setIsPopupOpen] = useState(false)
+            const [popupPosition, setPopupPosition] = useState<{
+                x: number
+                y: number
+            } | null>(null)
 
-                 const closePopup = () => {
-                     setIsPopupOpen(false)
-                     setPopupPosition(null)
-                 }
+            const handleEditClick = (event: React.MouseEvent<SVGSVGElement>) => {
+                const rect = event.currentTarget.getBoundingClientRect()
+                setPopupPosition({
+                    x: rect.right + window.scrollX - 20,
+                    y: rect.bottom + window.scrollY + 40,
+                })
+                setIsPopupOpen(true)
+            }
+
+            const closePopup = () => {
+                setIsPopupOpen(false)
+                setPopupPosition(null)
+            }
             return (
                 <div className="flex items-center justify-center gap-x-4 relative">
-                    <Pencil onClick={handleEditClick} className="text-primary-500 w-5 h-5 cursor-pointer" />
+                    <Pencil
+                        onClick={handleEditClick}
+                        className="text-primary-500 w-5 h-5 cursor-pointer"
+                    />
                     {isPopupOpen && popupPosition && (
                         <EditPopUpModal
                             isModalOpen={isPopupOpen}
                             onClose={closePopup}
                             popupPosition={popupPosition}
                         >
-                            <EditButtons/>
+                            <EditButtons />
                         </EditPopUpModal>
                     )}
                 </div>
