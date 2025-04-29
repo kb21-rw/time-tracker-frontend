aeb389ae822f3737540644888006b03965e668ff
manzitresor <manzitre@gmail.com>
2025-04-29 13:13:58 +0200

    refactor: change text of the buttons to center

--- src/components/ui/EditButtons.tsx
+++ src/components/ui/EditButtons.tsx
@@ -3,10 +3,10 @@ import Button from '../shared/ui/Button'
 export default function EditButtons() {
     return (
         <div className="flex  px-3 py-4 gap-2">
-            <Button className="w-full text-left px-2 py-2 justify-start font-normal cursor-pointer">
+            <Button className="w-full text-center  px-2 py-2 justify-start font-normal cursor-pointer">
                 Edit Client
             </Button>
-            <Button className="w-full text-left px-2 py-2 justify-start font-normal cursor-pointer">
+            <Button className="w-full text-center  px-2 py-2 justify-start font-normal cursor-pointer">
                 Edit Project
             </Button>
         </div>
