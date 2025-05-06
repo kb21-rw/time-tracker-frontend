33336aa693e3f276e3f0cba03e6c841932cd4354
manzitresor <manzitre@gmail.com>
2025-05-06 15:47:00 +0200

    chore: add click functionality on edit project button

--- src/components/ui/EditButtons.tsx
+++ src/components/ui/EditButtons.tsx
@@ -1,7 +1,12 @@
 import Button from '../shared/ui/Button'
 import { EditButtonsProps } from '../../util/interfaces'
 
-export default function EditButtons({ setClientModal, rowData, setSelectedRow }: EditButtonsProps) {
+export default function EditButtons({
+    setProjectModal,
+    setClientModal,
+    rowData,
+    setSelectedRow,
+}: EditButtonsProps) {
     return (
         <div className="flex px-3 py-4 gap-2">
             <Button
@@ -17,7 +22,17 @@ export default function EditButtons({ setClientModal, rowData, setSelectedRow }:
             >
                 Edit client
             </Button>
-            <Button className="w-full text-center px-2 py-2 justify-start font-normal cursor-pointer">
+            <Button
+                className="w-full text-center px-2 py-2 justify-start font-normal cursor-pointer"
+                onClick={() => {
+                    if (rowData && setSelectedRow) {
+                        setSelectedRow(rowData)
+                    }
+                    if (setProjectModal) {
+                        setProjectModal(true)
+                    }
+                }}
+            >
                 Edit project
             </Button>
         </div>
