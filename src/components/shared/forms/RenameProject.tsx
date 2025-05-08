7a16085ec5dee28704ab122d8f9d73fb496225e8
manzitresor <manzitre@gmail.com>
2025-05-08 16:56:54 +0200

    chore: get clients to select from backend

--- src/components/shared/forms/RenameProject.tsx
+++ src/components/shared/forms/RenameProject.tsx
@@ -1,56 +1,77 @@
-import { RootState } from '@/redux/store'
-import { clientSchema } from '@/schema/modal'
+import { Form, useParams } from 'react-router-dom'
+import Input from '../ui/Input'
+import Button from '../ui/Button'
+import { ProjectSchema } from '@/schema/modal'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import { useSelector } from 'react-redux'
-import Button from '../ui/Button'
-import Input from '../ui/Input'
+import { RootState, AppDispatch } from '@/redux/store'
+import { useSelector, useDispatch } from 'react-redux'
+import { getWorkspaceClients } from '@/redux/slice/clientSlice'
+import { useEffect } from 'react'
+import DownArrow from '@/assets/icons/DownArrow'
 import { RenameProjectProps } from '@/util/interfaces'
 
 export default function RenameProject({ projectName }: RenameProjectProps) {
-    const { loading, error } = useSelector((state: RootState) => state.workspaces)
+    const { id } = useParams<{ id: string }>()
+    const dispatch = useDispatch<AppDispatch>()
+    const { clients, error: clientsError } = useSelector((state: RootState) => state.clients)
+
+    useEffect(() => {
+        dispatch(getWorkspaceClients(id!))
+    }, [dispatch])
 
     const {
         register,
         formState: { errors, isValid },
     } = useForm({
-        resolver: zodResolver(clientSchema),
+        resolver: zodResolver(ProjectSchema),
         mode: 'all',
-        defaultValues: { name: projectName || '' },
+        defaultValues: { client: '', name: projectName || '' },
     })
 
     return (
-        <form className="w-full flex flex-col items-center">
+        <Form className="flex flex-col">
             <div className="w-full max-w-3xl font-inter pb-6 text-xl">
                 <div className="relative md:w-1/2">
-                    <select className="w-full appearance-none bg-transparent text-black text-lg font-normal pr-8 py-1 focus:outline-none cursor-pointer">
-                        <option value="project">Select Client
-
+                    <select
+                        {...register('client')}
+                        className="w-full appearance-none bg-transparent text-black text-lg font-normal pr-8 py-1 focus:outline-none cursor-pointer"
+                    >
+                        <option value="" disabled hidden>
+                            Select Client
                         </option>
-                        <option value="task">Task</option>
+                        {clients.length > 0 ? (
+                            clients.map(client => (
+                                <option className="mx-6" key={client.id} value={client.id}>
+                                    &nbsp;{client.name}
+                                </option>
+                            ))
+                        ) : (
+                            <option>No clients</option>
+                        )}
                     </select>
+
+                    <DownArrow className="pointer-events-none absolute right-0 top-1/2 -translate-y-1/2" />
                 </div>
-                <div className="w-full">
-                    <Input
-                        id="project-name"
-                        type="text"
-                        label="Rename Project"
-                        register={register('name')}
-                        placeholder={`${projectName || 'new name'}`}
-                        error={errors.name}
-                        tabIndex={-1}
-                        className="no-select-on-focus"
-                    />
-                </div>
-                {error && (
+                {clientsError && (
                     <p className="text-red-500 text-sm mt-2">
-                        {typeof error === 'string' ? error : JSON.stringify(error)}
+                        {typeof clientsError === 'string'
+                            ? clientsError
+                            : JSON.stringify(clientsError)}
                     </p>
                 )}
-                <Button className="w-1/2" disabled={!isValid} isLoading={loading}>
-                    Save
-                </Button>
             </div>
-        </form>
+            <Input
+                id="name"
+                label="Rename Project"
+                placeholder={`${projectName || 'new name'}`}
+                error={errors.name}
+                register={register('name')}
+            />
+
+            <Button className="self-center w-1/2" disabled={!isValid}>
+                Save
+            </Button>
+        </Form>
     )
 }
