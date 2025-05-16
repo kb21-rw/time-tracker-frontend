5dca350e74af5062ba7dffef2b0ee8c5cb9e80bf
deborah <deborahkamikazi9@gmail>
2025-05-16 19:06:39 +0200

    feat: create client intergration

--- src/components/shared/forms/CreateClient.tsx
+++ src/components/shared/forms/CreateClient.tsx
@@ -3,21 +3,51 @@ import Input from '../ui/Input'
 import z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import { useSelector } from 'react-redux'
-import { RootState } from '@/redux/store'
+import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react'
+import { AppDispatch, RootState } from '@/redux/store'
 import { clientSchema } from '@/schema/modal'
+import { createClient, getWorkspaceClients } from '@/redux/slice/clientSlice'
+import toast from 'react-hot-toast'
+import { handleAxiosError } from '@/util/helpers'
+import { AxiosError } from 'axios'
+import { useParams } from 'react-router-dom'
 
 type clientData = z.infer<typeof clientSchema>
 function CreateClient() {
+    const [createClientModal, setCreateClientModal] = useState(true)
+    const { id } = useParams<{ id: string }>()
+    const dispatch = useDispatch<AppDispatch>()
     const { loading } = useSelector((state: RootState) => state.workspaces)
     const {
         register,
         handleSubmit,
         formState: { errors, isValid },
     } = useForm<clientData>({ resolver: zodResolver(clientSchema), mode: 'all' })
-    const onSubmit = (data: clientData) => data
+
+    const handleCreateClient = async (data: clientData) => {
+        try {
+            const { meta: responseData } = await dispatch(
+                createClient({
+                    workspaceId: id!,
+                    name: data.name,
+                }),
+            )
+
+            if (responseData.requestStatus === 'fulfilled') {
+                dispatch(getWorkspaceClients(id!))
+                toast.success('You have successfully created a new Project')
+                setCreateClientModal(false)
+            } else {
+                toast.error('Failed to create a new project')
+            }
+        } catch (error) {
+            handleAxiosError(error as AxiosError)
+        }
+    }
+
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(handleCreateClient)}>
             <Input
                 label="Enter Client Name"
                 placeholder="Enter Client Name"
--- src/redux/slice/clientSlice.ts
+++ src/redux/slice/clientSlice.ts
@@ -21,12 +21,46 @@ export const getWorkspaceClients = createAsyncThunk(
         }
     },
 )
+
+export const createClient = createAsyncThunk(
+    'client',
+    async (
+        { workspaceId, name }: { workspaceId: string; name: string },
+        { rejectWithValue },
+    ) => {
+        try {
+            const response = await api.post(
+                `/workspaces/${workspaceId}/clients`,
+                { name },
+            )
+            return response.data
+        } catch (error: any) {
+            const errorMessage = error.response?.data?.message || 'Creating client failed'
+            return rejectWithValue(
+                typeof errorMessage === 'string' ? errorMessage : JSON.stringify(errorMessage),
+            )
+        }
+    },
+)
+
 const ClientSlice = createSlice({
     name: 'clients',
     initialState,
     reducers: {},
     extraReducers: builder => {
         builder
+         .addCase(createClient.pending, state => {
+                        state.loading = true
+                        state.error = null
+                    })
+                    .addCase(createClient.fulfilled, (state, action) => {
+                        state.loading = false
+                        state.clients = action.payload.projects
+                    })
+                    .addCase(createClient.rejected, (state, action) => {
+                        state.loading = false
+                        state.error = action.payload
+                    })
             .addCase(getWorkspaceClients.pending, state => {
                 state.loading = true
                 state.error = null
