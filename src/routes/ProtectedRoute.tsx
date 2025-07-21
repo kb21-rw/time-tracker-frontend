c01a36e8898928fdac96f65fa2421888c9a6d0a4
manzitresor <manzitre@gmail.com>
2025-07-21 11:41:59 +0200

    chore: get role from local storage and protect routes based on the role

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,10 +1,17 @@
+import { ProtectedRouteProps } from '@/util/interfaces'
 import { Navigate, Outlet } from 'react-router-dom'
 
-export function ProtectedRoute() {
+export function ProtectedRoute({allowedRoles}: ProtectedRouteProps) {
     const isAuthenticated = !!localStorage.getItem('token')
+    const user = localStorage.getItem('user')
+    const userRole = user ? JSON.parse(user).roles : null
 
     if (!isAuthenticated) {
         return <Navigate to="/login" replace />
+    }
+
+    if( allowedRoles && (!userRole || !allowedRoles.includes(userRole))){
+        return <Navigate to="/dashboard" replace />
     }
 
     return <Outlet />
