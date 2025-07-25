4a1bf3c2fb177b61bf71572e7d9384f34d5f819c
manzitresor <manzitre@gmail.com>
2025-07-25 09:17:49 +0200

    chore: add functionality of getting workspace for member user in tracker page

--- src/pages/TimeTracker.tsx
+++ src/pages/TimeTracker.tsx
@@ -1,27 +1,58 @@
 import TimeLogsGroup from '@/components/card/TimeLogsGroup'
 import TimeTrackerHeader from '@/components/ui/TimeTrackerHeader'
 import { getUserTimeLogs } from '@/redux/slice/timeLogsSlice'
+import { getWorkspacesByUser } from '@/redux/slice/workspaceSlice'
 import { AppDispatch, RootState } from '@/redux/store'
 import { formatTimeLogs } from '@/util/helpers'
 import { OutletContextType } from '@/util/interfaces'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { useOutletContext } from 'react-router-dom'
 
 export default function TimeTracker() {
-    const { id } = useOutletContext<OutletContextType>()
+    const outletContext = useOutletContext<OutletContextType>()
     const dispatch = useDispatch<AppDispatch>()
     const { timeLogs } = useSelector((state: RootState) => state.timeLog)
+    const [workspaceInfo, setWorkspaceInfo] = useState({id: '', workspaceName: ''})
+    const { workspaces, loading } = useSelector((state: RootState) => state.workspaces)
+    const user = JSON.parse(localStorage.getItem('user') || '{}')
+    const isAdmin = user.roles === 'Admin'
+    const id = outletContext?.id
+    const workspaceName = outletContext?.workspaceName
+
+    useEffect(()=> {
+        if (!isAdmin) {
+            dispatch(getWorkspacesByUser())
+        }
+    },[dispatch, isAdmin])
 
     useEffect(() => {
-        dispatch(getUserTimeLogs(id!))
-    }, [dispatch, id])
+        if (!isAdmin && workspaces.length > 0) {
+            const workspace = workspaces[0]
+            setWorkspaceInfo({
+                id: workspace.id,
+                workspaceName: workspace.name,
+            })
+        } else if (isAdmin && id) {
+            setWorkspaceInfo({
+                id: id,
+                workspaceName: workspaceName,
+            })
+            dispatch(getUserTimeLogs(id))
+        }
+    }, [dispatch, id, workspaces, isAdmin])
+
+
+    // To be style
+     if (loading) {
+         return <div>Loading workspace...</div>
+     }
 
     const formattedTimelogs = formatTimeLogs(timeLogs)
     return (
         <div className="bg-white h-full">
-            <TimeTrackerHeader />
+            <TimeTrackerHeader id={workspaceInfo.id} workspaceName={workspaceInfo.workspaceName}/>
             <TimeLogsGroup timeLogs={formattedTimelogs} />
         </div>
     )
