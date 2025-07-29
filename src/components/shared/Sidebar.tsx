adf328dc0bd7208c161fd0f2443936050108c594
manzitresor <manzitre@gmail.com>
2025-07-29 11:22:35 +0200

    fix: follow the kebab naming convention

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -9,7 +9,7 @@ import SidebarToggle from '../../assets/icons/SidebarToggle'
 import Tracker from '@/assets/icons/Tracker'
 import { useIsMobile } from '@/hooks/use-mobile'
 import Logo from '../../assets/icons/Logo'
-import userProfile from '../../assets/images/ix_user-profile-filled.svg'
+import userProfile from '../../assets/images/ix-user-profile-filled.svg'
 import { UserRole } from '../../util/interfaces'
 
 export default function Sidebar() {
