a9eb4803b5954b975811da6bd39c6493d4aa0271
Gihozo23 <christellegihozo23@gmail.com>
2025-08-08 12:24:26 +0200

    refactor: implement suggested change

--- src/components/card/TimeEntryCard.tsx
+++ src/components/card/TimeEntryCard.tsx
@@ -41,13 +41,11 @@ export default function TimeEntryCard({
             </div>
             <div className="text-right">{duration}</div>
             <div className="flex justify-center items-center">
-                {endTime !== 'Running' && (
-                    <button className="text-primary-500" onClick={() => setIsModalOpen(true)}>
-                        <Pencil className="w-5 h-5" />
-                    </button>
-                )}
+                <button className="text-primary-500" onClick={() => setIsModalOpen(true)}>
+                    <Pencil className="w-5 h-5" />
+                </button>
             </div>
-            {isModalOpen && endTime !== 'Running' && (
+            {isModalOpen && (
                 <EditTimeLog
                     isModalOpen={isModalOpen}
                     setIsModalOpen={setIsModalOpen}
--- src/redux/slice/timeLogsSlice.ts
+++ src/redux/slice/timeLogsSlice.ts
@@ -1,5 +1,5 @@
 import api from '@/lib/api'
-import { StartTimerPayload, TimeLogState, TimeLogEntryValues } from '@/util/interfaces'
+import { StartTimerPayload, TimeLogState, TimeLogEntryValues, TimeLog } from '@/util/interfaces'
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 const initialState: TimeLogState = {
@@ -180,9 +180,8 @@ const TimeLogSlice = createSlice({
                 state.loading = true
                 state.error = null
             })
-            .addCase(startTimerAPI.fulfilled, (state, action) => {
+            .addCase(startTimerAPI.fulfilled, state => {
                 state.loading = false
-                state.timeLogs.push(action.payload)
             })
             .addCase(startTimerAPI.rejected, (state, action) => {
                 state.loading = false
