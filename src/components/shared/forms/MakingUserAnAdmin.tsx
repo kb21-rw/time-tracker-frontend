b494a0e810d0f95bc40863d33bddbae601bca789
deborah <deborahkamikazi9@gmail>
2025-08-18 15:15:22 +0200

    feat: pop up form

---
+++ src/components/shared/forms/MakingUserAnAdmin.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '../ui/Button'
+
+function MakingUserAnAdmin() {
+    return (
+        <div>
+            <Button className="w-1/2">Confirm</Button>
+            <Button className="w-1/2">Cancel</Button>
+        </div>
+    )
+}
+
+export default MakingUserAnAdmin
