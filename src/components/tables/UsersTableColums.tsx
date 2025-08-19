9648eb549324f09472597be67261404ffcf0b6ea
deborah <deborahkamikazi9@gmail>
2025-08-19 15:16:44 +0200

    fix: resolving with prettier

--- src/components/tables/UsersTableColums.tsx
+++ src/components/tables/UsersTableColums.tsx
@@ -18,12 +18,12 @@ export function usersTableColumns(onOpenAdminModal: () => void): ColumnDef<Table
             header: 'Actions',
             cell: () => (
                 <div className="flex items-center justify-center gap-x-4">
-                  <button>
-                    <UserAddIcon
-                        className="text-primary-500 w-5 h-5 cursor-pointer"
-                        onClick={onOpenAdminModal}
-                    />
-                  </button>
+                    <button>
+                        <UserAddIcon
+                            className="text-primary-500 w-5 h-5 cursor-pointer"
+                            onClick={onOpenAdminModal}
+                        />
+                    </button>
                     <Trash2 className="text-accent-500 w-5 h-5 cursor-pointer" />
                 </div>
             ),
