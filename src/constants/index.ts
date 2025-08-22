383ed932f38b7bf9dd8482c286271d3c7fed9610
Gihozo23 <christellegihozo23@gmail.com>
2025-08-22 09:18:11 +0200

    refactor: create a time constants enum

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,9 @@
 export const API_URL = import.meta.env.VITE_API_URL
 
 // Time constants
-export const SECONDS_PER_MINUTE = 60
-export const MINUTES_PER_HOUR = 60
-export const HOURS_PER_DAY = 24
-export const MS_PER_SECOND = 1000
+export enum TimeConstants {
+    SECONDS_PER_MINUTE = 60,
+    MINUTES_PER_HOUR = 60,
+    HOURS_PER_DAY = 24,
+    MS_PER_SECOND = 1000,
+}
