2a8f90ff9dde4b461016c9d4b79429ad2cd5c27c
manzitresor <manzitre@gmail.com>
2025-09-04 17:01:26 +0200

    refactor: disable button when user is already admin

--- src/components/tables/UsersTableColums.tsx
+++ src/components/tables/UsersTableColums.tsx
@@ -20,18 +20,18 @@ export function usersTableColumns(
             header: 'Actions',
             cell: ({ row }) => {
                 const user = row.original
+                const isAdmin = user.roles === 'Admin'
                 return (
                     <div className="flex items-center justify-center gap-x-4">
-                        {user.roles !== 'Admin' && (
-                            <button
-                                type="button"
-                                onClick={() => onOpenAdminModal(user.id)}
-                                className="text-primary-500 w-5 h-5 cursor-pointer"
-                                aria-label="Make admin"
-                            >
-                                <UserAddIcon />
-                            </button>
-                        )}
+                        <button
+                            type="button"
+                            onClick={() => onOpenAdminModal(user.id)}
+                            className={`text-primary-500 w-5 h-5 cursor-pointer ${isAdmin ? 'opacity-50 cursor-not-allowed' : ''}`}
+                            aria-label="Make admin"
+                            disabled={isAdmin}
+                        >
+                            <UserAddIcon />
+                        </button>
                         <button
                             type="button"
                             className="text-accent-500 w-5 h-5 cursor-pointer"
