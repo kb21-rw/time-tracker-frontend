bd4a1450fcc66f8f853751d4fbb3a794b32c9289
manzitresor <manzitre@gmail.com>
2025-09-04 13:07:58 +0200

    refactor: add conditional rendering for makeAdmin button

--- src/components/tables/UsersTableColums.tsx
+++ src/components/tables/UsersTableColums.tsx
@@ -18,26 +18,31 @@ export function usersTableColumns(
         {
             accessorKey: 'actions',
             header: 'Actions',
-            cell: ({ row }) => (
-                <div className="flex items-center justify-center gap-x-4">
-                    <button
-                        type="button"
-                        onClick={() => onOpenAdminModal(row.original.id)}
-                        className=" text-primary-500 w-5 h-5 cursor-pointer"
-                        aria-label="Make admin"
-                    >
-                        <UserAddIcon />
-                    </button>
-                    <button
-                        type="button"
-                        className="text-accent-500 w-5 h-5 cursor-pointer"
-                        onClick={() => onOpenAdminModal(row.original.id)}
-                        aria-label="Remove user"
-                    >
-                        <Trash2 />
-                    </button>
-                </div>
-            ),
+            cell: ({ row }) => {
+                const user = row.original
+                return (
+                    <div className="flex items-center justify-center gap-x-4">
+                        {user.roles !== 'Admin' && (
+                            <button
+                                type="button"
+                                onClick={() => onOpenAdminModal(user.id)}
+                                className="text-primary-500 w-5 h-5 cursor-pointer"
+                                aria-label="Make admin"
+                            >
+                                <UserAddIcon />
+                            </button>
+                        )}
+                        <button
+                            type="button"
+                            className="text-accent-500 w-5 h-5 cursor-pointer"
+                            onClick={() => onOpenAdminModal(user.id)}
+                            aria-label="Remove user"
+                        >
+                            <Trash2 />
+                        </button>
+                    </div>
+                )
+            },
         },
     ]
 }
--- src/util/interfaces.ts
+++ src/util/interfaces.ts
@@ -67,10 +67,11 @@ export interface DataTableProps<TData, TValue> {
     loading: boolean
 }
 export type TableUser = {
-    id: string
-    names: string
+    id: number
+    fullName: string
     email: string
     actions?: string
+    roles: string
 }
 export interface Project {
     id: string
