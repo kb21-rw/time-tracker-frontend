eb8638bf1b08b985859c9734954dd9980200f65c
uwumukiza123 <uwumukiza123>
2025-09-12 12:06:25 +0200

    feat: add cypress configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,10 +5,10 @@ export default defineConfig({
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
-        env: {
-            VITE_API_URL: process.env.VITE_API_URL,
-            VITE_API_VERSION: process.env.VITE_API_VERSION,
-            VITE_APP_NAME: process.env.VITE_APP_NAME,
-        },
+    },
+    env: {
+        VITE_API_URL: process.env.VITE_API_URL,
+        VITE_API_VERSION: process.env.VITE_API_VERSION,
+        VITE_APP_NAME: process.env.VITE_APP_NAME,
     },
 })
